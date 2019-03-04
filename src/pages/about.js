65f7836d4a5e66a474598ea2e269bd658f96d5b8
StellrWeb <stellr@stellrweb.com>
2019-03-04 15:41:01 -0700

    initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => (
+  <div>
+    <h1>This is the about page</h1>
+    <Link to="/">Go Home</Link>
+  </div>
+)
+
+export default AboutPage
